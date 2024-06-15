f51326d58db9cbce3b11b73c3725234bab0ed106
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-06-15 02:24:55 -0500

    Update fasta.sh

--- Scripts/fasta.sh
+++ Scripts/fasta.sh
@@ -7,17 +7,4 @@ cd $PBS_O_WORKDIR
 
 module load sratoolkit/2.8.2
 
-#Single end
-#for i in $(more ../SraRunInfo.csv | sed 's/,/\t/g' | cut -f 1,16 | grep -i single | cut -f 1)
-#do
-#fastq-dump ../$i
-#done
-
-#Paired end
-#for i in $(more ../SraRunInfo.csv | sed 's/,/\t/g' | cut -f 1,16 | grep -i paired | cut -f 1)
-#do 
-#fastq-dump --split-files ../$i
-#done
-
-
 fastq-dump --split-files SRR*
