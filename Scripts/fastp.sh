c0856e5e729986a8d3f7c6069f5634d0f9c4a8c6
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-06-15 02:28:25 -0500

    Update fastp.sh

--- Scripts/fastp.sh
+++ Scripts/fastp.sh
@@ -8,13 +8,8 @@ cd $PBS_O_WORKDIR
 module load fastp/0.20
 
 #SE
-for i in $(more Sra.RunInfo.csv | sed 's/,/\t/g' | cut -f 2,17 | grep SINGLE | cut -f 1 | sed 's/^.//g' | sed 's/.$//')
+for i in $(ls | grep _1 | sed 's/_1\.fastq//g')
 do
 fastp -i ${i}_1.fastq -o out.${i}_1.fastq -q 30
 done
 
-#PE
-for i in $(more Sra.RunInfo.csv | sed 's/,/\t/g' | cut -f 2,17 | grep PAIRED | cut -f 1 | sed 's/^.//g' | sed 's/.$//')
-do
-fastp -i ${i}_1.fastq -I ${i}_2.fastq -o out.${i}_1.fastq -O out.${i}_2.fastq -q 30
-done
